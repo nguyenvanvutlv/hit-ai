a68e57168e37446c781cd273261d02a973b482d8
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2022-09-18 10:25:33 +0700

    fix

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hitai.wsgi --log-file -
--- hitai/settings.py
+++ hitai/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l17p0hd8^*b@j&cm8ytks1+#*%)!9_14li@bi*6y@olvxz-ws9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hit-ai.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
