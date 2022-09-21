fc23528a80aca76059a771bc2d3095af2c4474a1
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2022-09-21 07:53:25 +0700

    update insightface

--- camera/getcamera.py
+++ camera/getcamera.py
@@ -2,40 +2,33 @@ from pathlib import Path
 from imutils.video import VideoStream
 import imutils
 import cv2, os, urllib.request
+import numpy as np
 from django.conf import settings
-from .retinaface import Face_Recognition
 from camera.models import Image
+from insightface.app import FaceAnalysis
 
 
-def getImageFromDatabase(img):
-    resp = Face_Recognition.verify_database(img)
-    if resp == None:
-    	return None, None, None
-    labels = resp['label']
-    area = resp['facial_area']
-    distance = resp['distance']
-    if distance > 0.5:
-    	labels = "Unknown"
-    return labels, area, distance
 
     
 class VideoCamera(object):
     def __init__(self):
         # self.person = cv2.imread("img/main.jpg")
         self.video = cv2.VideoCapture(0)
-    
+        self.app = FaceAnalysis(name = 'buffalo_sc')
+        self.app.prepare(ctx_id=0, det_size=(640, 640))
+        
     def __del__(self):
         self.video.release()
     
     def get_frame(self):
         success, image = self.video.read()
         frame_flip = cv2.flip(image, 1)
-        frame_flip = cv2.resize(frame_flip, (640, 480))
-        labels, area, distance = getImageFromDatabase(frame_flip)
-        print(labels, distance)
-        if labels != None:
-        	cv2.rectangle(frame_flip, (area[0], area[1]), (area[2], area[3]), (0, 255, 0), 4)
-        	cv2.putText(frame_flip, labels, (area[0], area[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        faces = self.app.get(frame_flip)
+        
+        for index, value in enumerate(faces):
+            points = value['bbox'].astype(np.int)
+            frame_flip = cv2.rectangle(frame_flip, (points[0], points[1]), (points[2], points[3]), (0, 255, 0), 4)
+            cv2.putText(frame_flip, "A", (points[0], points[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
         ret, jpeg = cv2.imencode('.jpg', frame_flip)
         
         return jpeg.tobytes()
