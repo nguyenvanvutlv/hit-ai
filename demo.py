067772336de1658a56e15767f05b7fa89b4e2684
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2022-09-23 10:53:29 +0700

    file demo

--- demo.py
+++ demo.py
@@ -1,11 +1,42 @@
-from pathlib import Path
-import imutils
-import cv2, os, urllib.request
+import cv2, json, os, time
 import numpy as np
-from insightface.app import FaceAnalysis
-from retinaface.commons import distance as dst
-from deepface import DeepFace
-from deepface.basemodels import ArcFace
+from camera.hawk_eyes.face import RetinaFace, ArcFace, Landmark
+from camera.hawk_eyes.tracking import BYTETracker
+
+
+"""
+        load model
+"""
+# ---------------------------------------------- #
+retina = RetinaFace(model_name='retina_s')
+bt = BYTETracker()
+arc = ArcFace(model_name='arcface_s')
+landmark = Landmark()
+# ---------------------------------------------- #
+
+
+# ---------------------------------------------- #
+"""
+        load database
+"""
+database_emb = {
+    'userID': [],
+    'embs': []
+}
+img_data_list = os.listdir("img/source")
+for i in range(len(img_data_list)):
+    img_path = os.path.join("img/source", img_data_list[i])
+    img = cv2.imread(img_path)
+    # print(img_path)
+    label = img_data_list[i].split('.')[0]
+    fbox, kpss = retina.detect(img)
+    emb = arc.get(img, kpss[0])
+    database_emb['embs'].append(emb)
+    database_emb['userID'].append(label)
+print("Load data done!")
+# ---------------------------------------------- #
+
+
 
 
 def draw_border(img, pt1, pt2, color, thickness, r, d):
@@ -31,82 +62,77 @@ def draw_border(img, pt1, pt2, color, thickness, r, d):
     cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
     cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
     cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
-    
-# đường dẫn database -> img/source/database.txt
-def verify_database(embedding1, region, distance_metric = 'cosine', database = 'img/source/database.txt'):
-    resp_objects = []
-    with open(database, 'r') as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            line = line.split()
-            label = line[0]
-            embedding2 = list(map(float, line[1:]))
-            if distance_metric == 'cosine':
-                distance = dst.findCosineDistance(embedding1, embedding2)
-            elif distance_metric == 'euclidean':
-                distance = dst.findEuclideanDistance(embedding1, embedding2)
-            elif distance_metric == 'euclidean_l2':
-                distance = dst.findEuclideanDistance(dst.l2_normalize(embedding1), dst.l2_normalize(embedding2))
-            else:
-                raise ValueError("Invalid distance_metric passed - ", distance_metric)
-            
-            distance = np.float64(distance) #causes trobule for euclideans in api calls if this is not set (issue #175)
-            # print(distance)
-            threshold = dst.findThreshold(distance_metric)
-            # print(threshold)
-            
-            if distance <= threshold:
-                resp_obj = {
-					"label": label
-					, "facial_area": region
-					, "distance": distance
-					, "threshold": threshold
-					, "similarity_metric": distance_metric
-				}
-                resp_objects.append(resp_obj)
-    if len(resp_objects) == 0:
-        return None
-    
-    return min(resp_objects, key=lambda x: x['distance'])
 
-# ------------ SETUP CAM ------------#
-video = cv2.VideoCapture(0)
+current_frame = 0
+last_frame = 0
+
+
+
+video = cv2.VideoCapture("img/A.mp4")
 frame_width = int(video.get(3))
-frame_height = int(video.get(4)) 
-app = FaceAnalysis(name = 'buffalo_sc')
-app.prepare(ctx_id=0, det_size=(640, 640))
-model_arc = ArcFace.loadModel()
-# ------------ SETUP CAM ------------#
+frame_height = int(video.get(4))
+size = (frame_width, frame_height)
+_fps = video.get(cv2.CAP_PROP_FPS)
+result = cv2.VideoWriter('img/out.avi', 
+                         cv2.VideoWriter_fourcc(*'MJPG'),
+                         _fps, size)
 
 
 while True:
-    success, image = video.read()
-    frame_flip = cv2.flip(image, 1)
-    faces = app.get(frame_flip)
-    for index, value in enumerate(faces):
+    current_frame = time.time()
+    _, frame = video.read()
+    if _ != True:
+        break
+    # frame = cv2.flip(frame, 1)
+    bboxes, kpss = retina.detect(frame)
+    """
+        bboxes[i]: danh sách box có mặt 
+        kpss[i]  : danh sách phân tích mặt
+    """
+    boxes, tids = bt.predict(frame, bboxes)
+    """
+        boxes[i]: dạnh sách box có mặt di chuyển đến vị trí tiếp theo từ bboxes[i]
+    """
+    
+    tkpss = [None]*len(bboxes)
+    for i in range(len(boxes)):
+        min_d = 9e5
+        tb = boxes[i]
+        for j in range(len(bboxes)):
+            fb = bboxes[j]
+            d = abs(tb[0]-fb[0])+abs(tb[1]-fb[1]) + abs(tb[2]-fb[2])+abs(tb[3]-fb[3])
+            if d < min_d:
+                min_d = d
+                tkpss[i] = kpss[j]
+    
+    embs = []
+    ids = []
+    for tid, tbox, tkps in zip(tids, boxes, tkpss):
+            # print(localtime)
+        box = tbox[:4].astype(int)
+        land = landmark.get(frame, tbox)
+        angle = landmark.get_face_angle(frame, land, False)
+        if abs(angle) < 15:
+            embs.append(arc.get(frame, tkps))
+            ids.append(tid)
+        draw_border(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2, 10, 20)
         
-            points = value['bbox'].astype(np.int)
-
-            # ------------EMBEDDING---------------#
-            result = None            
-            copy_frame = frame_flip.copy()
-            copy_frame = copy_frame[points[1]: points[3], points[0]: points[2]]
-            result = DeepFace.represent(img_path = copy_frame, model_name = 'ArcFace', model = model_arc, enforce_detection = False)
-            result = verify_database(result, points)
-            # ------------EMBEDDING---------------#
-            
-            
-            # ----------------DRAW----------------#
-            label = "Unknown" if result == None else result['label']
-            draw_border(frame_flip, (points[0], points[1]), (points[2], points[3]), (0, 255, 0), 3, 10, 20)
-            cv2.putText(frame_flip, label, (points[0], points[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            # ----------------DRAW----------------#
-            
-            
-    cv2.imshow("a", frame_flip)
+    
+    if len(embs) > 0:
+        for i in embs:
+            dis_cosin = np.dot(database_emb['embs'], i) / (np.linalg.norm(database_emb['embs']) * np.linalg.norm(i))
+            label = "Stranger" if max(dis_cosin) <= 0.1 else str(database_emb['userID'][np.argmax(dis_cosin)])
+            cv2.putText(frame, label, (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    fps = 1/(current_frame - last_frame)
+    last_frame = current_frame
+    current_frame = time.time()
+    
+    cv2.putText(frame, 'fps: {}'.format(int(fps)),  (frame.shape[1]-150, frame.shape[0]-100), cv2.FONT_HERSHEY_DUPLEX, 1, (0,255,0), 2 )
+    
+    # cv2.imshow('image', frame)
+    result.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('x'):
         break
 video.release()
+result.release()
 cv2.destroyAllWindows()
